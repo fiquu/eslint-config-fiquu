2509802aece34013990405f7f963bf425cd80548
Santiago G. Marín <santiago.marin@dotstudio.io>
2020-03-16 18:50:15 -0300

    Fixed extends

--- typescript.js
+++ typescript.js
@@ -1,14 +1,14 @@
 const { join } = require('path');
 
 module.exports = {
-  extends: join(__dirname, 'index.js'),
+  extends: [
+    join(__dirname, 'index.js'),
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint'
-  ],
-  extends: [
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
   ],
   rules: {
     'node/no-unsupported-features/es-syntax': 'off', // Wont recognize TS features
