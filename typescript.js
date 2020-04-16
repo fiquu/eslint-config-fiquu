50408950a2924913aea51ac719aaed7ae1288556
Santiago G. Marín <santiago.marin@dotstudio.io>
2020-04-16 10:01:57 -0400

    RElaxed typescript rules

--- typescript.js
+++ typescript.js
@@ -11,6 +11,7 @@ module.exports = {
     '@typescript-eslint'
   ],
   rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off', // Allow TS to guess return type
     'node/no-unsupported-features/es-syntax': 'off', // Wont recognize TS features
     'node/no-missing-import': 'off' // Wont recognize .ts
   }
